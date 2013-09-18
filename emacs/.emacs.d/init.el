c24f53089b35fe2bf521ac93f3624532d59e04f0
David Leung <david@davidslab.com>
2013-09-19 06:41:52 +0800

    Initial commit

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,36 @@
+;; PACKAGES
+
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(package-initialize)
+
+(when (null package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(better-defaults clojure-mode clojure-test-mode paredit
+                                      idle-highlight-mode
+                                      rainbow-delimiters rainbow-mode
+                                      pastels-on-dark-theme
+                                      smex))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+;; ACTIVATIONS
+
+(setq smex-save-file (concat user-emacs-directory ".smex-items"))
+(smex-initialize)
+
+(global-set-key (kbd "M-x") 'smex)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(column-number-mode t)
+
+(load-theme 'pastels-on-dark t)
